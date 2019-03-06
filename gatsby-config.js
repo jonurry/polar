348b368d2e61e9e2594e47432229e3a01e84bfb4
Jon Urry <jon@urry.me>
2019-03-06 09:33:15 +0000

    Change title, description and author

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Polar`,
+    description: `Analyse your Strava activities against a Polar training plan.`,
+    author: `@jonurry`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,

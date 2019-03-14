097c8967510b5241c4ad4885b60e5fdc02c7b2bd
Jon Urry <jon@urry.me>
2019-03-14 15:44:17 +0000

    Refactor processing of heartrate data

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -1,53 +1,97 @@
-const filterByDates = (data, dateFrom, dateTo) => {
+// const stream = data=>{
+//   let stream = data
+//   return {
+//     filterByDates(dateFrom, dateTo) {
+//   return data.filter(edge => {
+//     // filter between specified dates
+//     return (
+//       edge.node.activity.start_date > dateFrom &&
+//       edge.node.activity.start_date < dateTo
+//     )
+//   })
+// },
+// removeActivitiesWithoutHeartrateData = data => {
+//   return data.filter(edge => {
+//     // filter activities where heartrate zone data does not exist
+//     return (
+//       edge.node.activity.zones.filter(zone => {
+//         return zone.type === "heartrate"
+//       }).length > 0
+//     )
+//   })
+// }
+//   }
+// }
+
+const extractTimeInEachZone = zones => {
+  return zones.map((zone, index) => {
+    // extract time in each zone
+    let o = {}
+    o[`Zone ${index + 1}`] = zone.time
+    return o
+  })
+}
+
+const filterActivitiesByDates = (data, dateFrom, dateTo) => {
   return data.filter(edge => {
     // filter between specified dates
-    // and where heartrate zone data exists
     return (
       edge.node.activity.start_date > dateFrom &&
-      edge.node.activity.start_date < dateTo &&
-      edge.node.activity.zones.filter(zone => {
-        return zone.type === "heartrate"
-      }).length > 0
+      edge.node.activity.start_date < dateTo
     )
   })
 }
 
-export const getActivityStreamData = (data, dateFrom, dateTo) => {
-  return filterByDates(data, dateFrom, dateTo).map(edge => {
-    // filter activity for heartrate zone bucket only
-    let hrZones = edge.node.activity.zones.filter(zone => {
-      return zone.type === "heartrate"
-    })
-    // return default zones if not defined
-    if (hrZones.length === 0)
-      return {
-        "Zone 1": 0,
-        "Zone 2": 0,
-        "Zone 3": 0,
-        "Zone 4": 0,
-        "Zone 5": 0,
-      }
-    // return time spent in each zone
-    return hrZones[0].distribution_buckets
-      .sort((a, b) => {
-        // Order the zones in the bucket
-        if (a.min < b.min) {
-          return -1
-        }
-        if (a.min > b.min) {
-          return 1
-        }
-        return 0
-      })
-      .map((zone, index) => {
-        // extract time in each zone
-        let o = {}
-        o[`Zone ${index + 1}`] = zone.time
-        return o
-      })
-      .reduce((acc, cur) => {
-        // merge zone objects into one
-        return Object.assign(acc, cur)
-      }, {})
+const getActivitiesWithHeartrateData = data => {
+  return data.filter(edge => {
+    // filter activities where heartrate zone data does not exist
+    return getHeartrateZones(edge.node.activity.zones).length > 0
   })
+}
+
+const getHeartrateZones = zones => {
+  return zones.filter(zone => {
+    return zone.type === "heartrate"
+  })
+}
+
+const getTimeSpentInEachZone = data => {
+  return data.map(edge => {
+    // return time spent in each zone
+    return mergeZones(
+      extractTimeInEachZone(
+        sortZonesInBucket(
+          getHeartrateZones(edge.node.activity.zones)[0].distribution_buckets
+        )
+      )
+    )
+  })
+}
+
+const mergeZones = zones => {
+  return zones.reduce((acc, cur) => {
+    // merge zone objects into one
+    return Object.assign(acc, cur)
+  }, {})
+}
+
+const sortZonesInBucket = bucket => {
+  return bucket.sort((a, b) => {
+    // Order the zones in the bucket
+    if (a.min < b.min) {
+      return -1
+    }
+    if (a.min > b.min) {
+      return 1
+    }
+    return 0
+  })
+}
+
+export const getActivityStreamData = (data, dateFrom, dateTo) => {
+  return getTimeSpentInEachZone(
+    getActivitiesWithHeartrateData(
+      filterActivitiesByDates(data, dateFrom, dateTo)
+    )
+  )
 }

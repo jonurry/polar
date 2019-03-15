f4c90ab04c1072c6fde05dc90cbd22af94b4542a
Jon Urry <jon@urry.me>
2019-03-15 13:50:17 +0000

    Move graphQL query from stream component to index page

--- src/components/graph.js
+++ src/components/graph.js
@@ -4,150 +4,111 @@ import { StaticQuery, Link, graphql } from "gatsby"
 // import { rhythm } from '../utils/typography';
 import { ResponsiveStream } from "@nivo/stream"
 import { linearGradientDef } from "@nivo/core"
-import { getActivityStreamData } from "../utils/parser"
 
-export default ({ dateFrom, dateTo, children }) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        allStravaActivity(
-          sort: { fields: [activity___start_date], order: ASC }
-        ) {
-          edges {
-            node {
-              activity {
-                name
-                start_date
-                distance
-                average_heartrate
-                average_watts
-                suffer_score
-                zones {
-                  score
-                  points
-                  type
-                  distribution_buckets {
-                    max
-                    min
-                    time
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    `}
-    render={data => (
-      <div
-        style={{ height: `70vh`, minHeight: `500px` }}
-        // css={css`
-        //   margin: 0 auto;
-        //   max-width: 700px;
-        //   padding: ${rhythm(2)};
-        //   padding-top: ${rhythm(1.5)};
-        // `}
-      >
-        <ResponsiveStream
-          data={getActivityStreamData(
-            data.allStravaActivity.edges,
-            dateFrom,
-            dateTo
-          )}
-          keys={["Zone 1", "Zone 2", "Zone 3", "Zone 4", "Zone 5"]}
-          margin={{
-            top: 50,
-            right: 110,
-            bottom: 50,
-            left: 60,
-          }}
-          axisTop={null}
-          axisRight={null}
-          axisBottom={null}
-          axisLeft={{
-            orient: "left",
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: "",
-            legendOffset: -40,
-          }}
-          enableGridX={false}
-          curve="catmullRom"
-          offsetType="silhouette"
-          fillOpacity={0.85}
-          borderColor="#000"
-          // color={["#000", "#444", "#888", "#bbb", "#eee"]}
-          defs={[
+export default ({ stream, dateFrom, dateTo, children }) => (
+  <div
+    style={{ height: `70vh`, minHeight: `500px` }}
+    // css={css`
+    //   margin: 0 auto;
+    //   max-width: 700px;
+    //   padding: ${rhythm(2)};
+    //   padding-top: ${rhythm(1.5)};
+    // `}
+  >
+    <ResponsiveStream
+      data={stream}
+      keys={["Zone 1", "Zone 2", "Zone 3", "Zone 4", "Zone 5"]}
+      margin={{
+        top: 50,
+        right: 110,
+        bottom: 50,
+        left: 60,
+      }}
+      axisTop={null}
+      axisRight={null}
+      axisBottom={null}
+      axisLeft={{
+        orient: "left",
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: "",
+        legendOffset: -40,
+      }}
+      enableGridX={false}
+      curve="catmullRom"
+      offsetType="silhouette"
+      fillOpacity={0.85}
+      borderColor="#000"
+      // color={["#000", "#444", "#888", "#bbb", "#eee"]}
+      defs={[
+        {
+          id: "dots",
+          type: "patternDots",
+          background: "#7BB0A8",
+          color: "#A7DBAB",
+          size: 3,
+          padding: 3,
+          stagger: true,
+        },
+        {
+          id: "lines2",
+          type: "patternLines",
+          background: "#A7DBAB",
+          color: "#7BB0A8",
+          rotation: -45,
+          lineWidth: 1,
+          spacing: 12,
+        },
+        {
+          id: "lines4",
+          type: "patternLines",
+          background: "#A7DBAB",
+          color: "#7BB0A8",
+          rotation: 45,
+          lineWidth: 1,
+          spacing: 12,
+        },
+        linearGradientDef("gradientZone3", [
+          { offset: 0, color: "#fff" },
+          { offset: 50, color: "#f00" },
+          { offset: 100, color: "#fff" },
+        ]),
+      ]}
+      fill={[
+        { match: { id: "Zone 1" }, id: "dots" },
+        { match: { id: "Zone 2" }, id: "lines2" },
+        { match: { id: "Zone 3" }, id: "gradientZone3" },
+        { match: { id: "Zone 4" }, id: "lines4" },
+        { match: { id: "Zone 5" }, id: "dots" },
+      ]}
+      dotSize={8}
+      dotBorderWidth={2}
+      dotBorderColor="inherit:brighter(0.7)"
+      animate={true}
+      motionStiffness={90}
+      motionDamping={15}
+      legends={[
+        {
+          anchor: "bottom-right",
+          direction: "column",
+          translateX: 100,
+          itemWidth: 80,
+          itemHeight: 20,
+          itemTextColor: "#999",
+          symbolSize: 12,
+          symbolShape: "circle",
+          effects: [
             {
-              id: "dots",
-              type: "patternDots",
-              background: "#7BB0A8",
-              color: "#A7DBAB",
-              size: 3,
-              padding: 3,
-              stagger: true,
+              on: "hover",
+              style: {
+                itemTextColor: "#000",
+              },
             },
-            {
-              id: "lines2",
-              type: "patternLines",
-              background: "#A7DBAB",
-              color: "#7BB0A8",
-              rotation: -45,
-              lineWidth: 1,
-              spacing: 12,
-            },
-            {
-              id: "lines4",
-              type: "patternLines",
-              background: "#A7DBAB",
-              color: "#7BB0A8",
-              rotation: 45,
-              lineWidth: 1,
-              spacing: 12,
-            },
-            linearGradientDef("gradientZone3", [
-              { offset: 0, color: "#fff" },
-              { offset: 50, color: "#f00" },
-              { offset: 100, color: "#fff" },
-            ]),
-          ]}
-          fill={[
-            { match: { id: "Zone 1" }, id: "dots" },
-            { match: { id: "Zone 2" }, id: "lines2" },
-            { match: { id: "Zone 3" }, id: "gradientZone3" },
-            { match: { id: "Zone 4" }, id: "lines4" },
-            { match: { id: "Zone 5" }, id: "dots" },
-          ]}
-          dotSize={8}
-          dotBorderWidth={2}
-          dotBorderColor="inherit:brighter(0.7)"
-          animate={true}
-          motionStiffness={90}
-          motionDamping={15}
-          legends={[
-            {
-              anchor: "bottom-right",
-              direction: "column",
-              translateX: 100,
-              itemWidth: 80,
-              itemHeight: 20,
-              itemTextColor: "#999",
-              symbolSize: 12,
-              symbolShape: "circle",
-              effects: [
-                {
-                  on: "hover",
-                  style: {
-                    itemTextColor: "#000",
-                  },
-                },
-              ],
-            },
-          ]}
-        />
-        {children}
-      </div>
-    )}
-  />
+          ],
+        },
+      ]}
+    />
+    {children}
+  </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,61 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import Graph from "../components/graph"
+import { getActivityStreamData } from "../utils/parser"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Polar" keywords={[`strava`, `polar`, `analysis`]} />
-    <p>Welcome to Polar.</p>
-    <p>Analyse your strava activities against a polar training plan.</p>
-    <Graph dateFrom={"2019-01-01"} dateTo={"2020-01-01"} />
-    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+const IndexPage = ({ data }) => {
+  let dateFrom = "2019-01-01"
+  let dateTo = "2020-01-01"
+  let stream = getActivityStreamData(
+    data.allStravaActivity.edges,
+    dateFrom,
+    dateTo
+  )
+  return (
+    <Layout>
+      <SEO title="Polar" keywords={[`strava`, `polar`, `analysis`]} />
+      <p>Welcome to Polar.</p>
+      <p>Analyse your strava activities against a polar training plan.</p>
+      <Graph stream={stream} dateFrom={dateFrom} dateTo={dateTo} />
+      {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div> */}
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allStravaActivity(sort: { fields: [activity___start_date], order: ASC }) {
+      edges {
+        node {
+          activity {
+            name
+            start_date
+            distance
+            average_heartrate
+            average_watts
+            suffer_score
+            zones {
+              score
+              points
+              type
+              distribution_buckets {
+                max
+                min
+                time
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
